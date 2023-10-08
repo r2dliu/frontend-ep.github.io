b5f27593c9d66eafeb20f1242e73623311dc14d7
David Liu <r2dliu@Davids-MBP.hsd1.ga.comcast.net>
2023-10-08 12:03:32 -0400

    borders

--- module6_solution/app.js
+++ module6_solution/app.js
@@ -4,12 +4,17 @@
     .controller("LunchCheckController", function($scope) {
         $scope.list=""
         $scope.showMessage = null
+        $scope.color = null
+        $scope.border = null
         $scope.checkTooMuch = function () {
             if ($scope.list === "") {
-
                 $scope.showMessage = "Please enter data first"
+                $scope.border = "border-red"
+                $scope.color = "text-red"
             } else {
                 $scope.showMessage = null
+                $scope.color = "text-green"
+                $scope.border = "border-green"
                 items = $scope.list.split(",")
                 // Filter out empty items: any items that are made up of only whitespace
                 // do NOT contribute towards the item count
